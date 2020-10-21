ab8693ebb3baf03ce56d451699a1600dabaf2008
LuigiBlood <luigiblood@gmail.com>
2020-10-21 17:53:22 +0200

    Add poor NDD to D64 conversion code

--- ddmaster/Program.cs
+++ ddmaster/Program.cs
@@ -9,7 +9,7 @@ namespace ddmaster
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("ddmaster v0.1");
+            Console.WriteLine("ddmaster v0.2");
             Console.WriteLine("---- by LuigiBlood");
             Console.WriteLine("");
 
@@ -19,6 +19,7 @@ namespace ddmaster
             string set_o = "";
             string set_ipladdr = "";
             string set_iplsize = "";
+            string set_ndd = "";
 
             if (args.Length == 0 && ((args.Length & 1) == 1))
             {
@@ -59,84 +60,169 @@ namespace ddmaster
                     i++;
                     set_o = args[i];
                 }
+                else if (args[i] == "-ndd")
+                {
+                    i++;
+                    set_ndd = args[i];
+                }
             }
 
             if (set_o == "")
                 set_o = "master.d64";
 
             //Check if required ones are found
-            if (set_cfg == "" || set_rom == "")
+            if (set_ndd == "" && set_cfg == "" && set_rom == "")
             {
                 Usage();
                 return;
             }
 
-            //Make disk
-            int cfg_disktype = -1;
-            int cfg_destcode = 0;
-            byte[] cfg_diskid;
-            ProcessCfg(set_cfg, out cfg_disktype, out cfg_destcode, out cfg_diskid);
-
-            uint cfg_ipladdr = uint.Parse(set_ipladdr, System.Globalization.NumberStyles.HexNumber);
-            int cfg_iplsize = bytetolba(cfg_disktype, int.Parse(set_iplsize), 24);
-
-            //Take ROM size
-            FileStream file_rom = new FileStream(set_rom, FileMode.Open);
-            if (cfg_disktype == -1)
+            //Make Disk Code
+            if (set_ndd == "")
             {
-                if (file_rom.Length < 26014080) cfg_disktype = 0;
-                else if (file_rom.Length < 34957440) cfg_disktype = 1;
-                else if (file_rom.Length < 43155520) cfg_disktype = 2;
-                else if (file_rom.Length < 50608320) cfg_disktype = 3;
-                else if (file_rom.Length < 57315840) cfg_disktype = 4;
-                else if (file_rom.Length < 62516480) cfg_disktype = 5;
-                else if (file_rom.Length < 64458560) cfg_disktype = 6;
+                //Make Disk from CFG and ROM
+                int cfg_disktype = -1;
+                int cfg_destcode = 0;
+                byte[] cfg_diskid;
+                ProcessCfg(set_cfg, out cfg_disktype, out cfg_destcode, out cfg_diskid);
+
+                uint cfg_ipladdr = uint.Parse(set_ipladdr, System.Globalization.NumberStyles.HexNumber);
+                int cfg_iplsize = bytetolba(cfg_disktype, int.Parse(set_iplsize), 24);
+
+                //Take ROM size
+                FileStream file_rom = new FileStream(set_rom, FileMode.Open);
+                if (cfg_disktype == -1)
+                {
+                    if (file_rom.Length < 26014080) cfg_disktype = 0;
+                    else if (file_rom.Length < 34957440) cfg_disktype = 1;
+                    else if (file_rom.Length < 43155520) cfg_disktype = 2;
+                    else if (file_rom.Length < 50608320) cfg_disktype = 3;
+                    else if (file_rom.Length < 57315840) cfg_disktype = 4;
+                    else if (file_rom.Length < 62516480) cfg_disktype = 5;
+                    else if (file_rom.Length < 64458560) cfg_disktype = 6;
+                }
+
+                if (file_rom.Length > 64458560)
+                {
+                    //TOO BIG!
+                    Console.WriteLine("ERROR: ROM FILE IS TOO BIG! (MAX SIZE 64458560 bytes)");
+                    return;
+                }
+
+                int block_rom_size = bytetolba(cfg_disktype, (int)file_rom.Length, 24);
+                int block_rom_byte_size = lbatobyte(cfg_disktype, block_rom_size, 24);
+
+                byte[] rom_data = new byte[block_rom_byte_size];
+                Array.Fill<byte>(rom_data, 0xFF);
+
+                file_rom.Read(rom_data, 0, (int)file_rom.Length);
+
+                //Make D64 file
+                FileStream file_out = new FileStream(set_o, FileMode.Create);
+
+                byte[] d64_sys_data = new byte[256];
+                byte[] d64_sys_id = new byte[256];
+                d64_sys_data[0x05] = (byte)cfg_disktype;
+                d64_sys_data[0x06] = (byte)(cfg_iplsize >> 8);
+                d64_sys_data[0x07] = (byte)(cfg_iplsize & 0xFF);
+
+                d64_sys_data[0x1C] = (byte)(cfg_ipladdr >> 24);
+                d64_sys_data[0x1D] = (byte)(cfg_ipladdr >> 16);
+                d64_sys_data[0x1E] = (byte)(cfg_ipladdr >> 8);
+                d64_sys_data[0x1F] = (byte)(cfg_ipladdr & 0xFF);
+
+                d64_sys_data[0xE0] = (byte)((block_rom_size - 1) >> 8);
+                d64_sys_data[0xE1] = (byte)((block_rom_size - 1) & 0xFF);
+                d64_sys_data[0xE2] = 0xFF;
+                d64_sys_data[0xE3] = 0xFF;
+                d64_sys_data[0xE4] = 0xFF;
+                d64_sys_data[0xE5] = 0xFF;
+
+                Array.Copy(cfg_diskid, d64_sys_id, cfg_diskid.Length);
+                d64_sys_id[0xE8] = (byte)cfg_destcode;
+
+                file_out.Write(d64_sys_data, 0, d64_sys_data.Length);
+                file_out.Write(d64_sys_id, 0, d64_sys_id.Length);
+                file_out.Write(rom_data, 0, rom_data.Length);
+
+                file_out.Close();
             }
-            
-            if (file_rom.Length > 64458560)
+            else
             {
-                //TOO BIG!
-                Console.WriteLine("ERROR: ROM FILE IS TOO BIG! (MAX SIZE 64458560 bytes)");
-                return;
+                //Make Disk from NDD Disk File
+                FileStream file_ndd = new FileStream(set_ndd, FileMode.Open);
+
+                //Make sure it is the right size
+                if (file_ndd.Length != 0x3DEC800)
+                {
+                    Console.WriteLine("ERROR: NDD FILE SIZE IS NOT VALID");
+                    file_ndd.Close();
+                    return;
+                }
+
+                //Make sure it is a retail disk (assumes the first block is correct for now)
+                for (int i = 0; i < 85; i++)
+                {
+                    file_ndd.Seek(i * 0xE8, SeekOrigin.Begin);
+                    uint temp = 0;
+                    temp = (uint)(file_ndd.ReadByte() & 0xFF);
+                    temp = (temp << 8) | (uint)(file_ndd.ReadByte() & 0xFF);
+                    temp = (temp << 8) | (uint)(file_ndd.ReadByte() & 0xFF);
+                    temp = (temp << 8) | (uint)(file_ndd.ReadByte() & 0xFF);
+
+                    if (temp != 0xE848D316 && temp != 0x2263EE56)
+                    {
+                        Console.WriteLine("ERROR: NDD SYSTEM DATA BLOCK 0 IS NOT VALID");
+                        file_ndd.Close();
+                        return;
+                    }
+                }
+
+                //Sys Data
+                byte[] d64_sys_data = new byte[256];
+                file_ndd.Seek(0, SeekOrigin.Begin);
+                file_ndd.Read(d64_sys_data, 0, 0xE8);
+
+                for (int i = 0; i < 5; i++)
+                    d64_sys_data[i] = 0;
+                d64_sys_data[5] &= 0x0F;
+                for (int i = 8; i < 0x1C; i++)
+                    d64_sys_data[i] = 0;
+                for (int i = 0x20; i < 0xE0; i++)
+                    d64_sys_data[i] = 0;
+                for (int i = 0xE6; i < 0xE8; i++)
+                    d64_sys_data[i] = 0;
+
+                byte disk_type = d64_sys_data[5];
+                ushort lba_rom_end = (ushort)((d64_sys_data[0xE0] << 8) | d64_sys_data[0xE1]);
+                ushort lba_ram_start = (ushort)((d64_sys_data[0xE2] << 8) | d64_sys_data[0xE3]);
+                ushort lba_ram_end = (ushort)((d64_sys_data[0xE4] << 8) | d64_sys_data[0xE5]);
+
+                //Disk ID
+                byte[] d64_sys_id = new byte[256];
+                file_ndd.Seek(0x43670, SeekOrigin.Begin);
+                file_ndd.Read(d64_sys_id, 0, 0xE8);
+
+                //ROM Area
+                byte[] d64_rom = new byte[lbatobyte(disk_type, lba_rom_end + 1, 24)];
+                file_ndd.Seek(0x738C0, SeekOrigin.Begin);
+                file_ndd.Read(d64_rom, 0, d64_rom.Length);
+
+                //RAM Area
+                byte[] d64_ram = new byte[lbatobyte(disk_type, lba_ram_end - lba_ram_start, lba_ram_start + 24)];
+                file_ndd.Seek(0x738C0 + lbatobyte(disk_type, lba_ram_start, 24), SeekOrigin.Begin);
+                file_ndd.Read(d64_ram, 0, d64_ram.Length);
+                file_ndd.Close();
+
+                //Write D64 File
+                FileStream file_out = new FileStream(set_o, FileMode.Create);
+                file_out.Write(d64_sys_data, 0, d64_sys_data.Length);
+                file_out.Write(d64_sys_id, 0, d64_sys_id.Length);
+                file_out.Write(d64_rom, 0, d64_rom.Length);
+                file_out.Write(d64_ram, 0, d64_ram.Length);
+
+                file_out.Close();
             }
-
-            int block_rom_size = bytetolba(cfg_disktype, (int)file_rom.Length, 24);
-            int block_rom_byte_size = lbatobyte(cfg_disktype, block_rom_size, 24);
-
-            byte[] rom_data = new byte[block_rom_byte_size];
-            Array.Fill<byte>(rom_data, 0xFF);
-
-            file_rom.Read(rom_data, 0, (int)file_rom.Length);
-
-            //Make D64 file
-            FileStream file_out = new FileStream(set_o, FileMode.Create);
-
-            byte[] d64_sys_data = new byte[256];
-            byte[] d64_sys_id = new byte[256];
-            d64_sys_data[0x05] = (byte)cfg_disktype;
-            d64_sys_data[0x06] = (byte)(cfg_iplsize >> 8);
-            d64_sys_data[0x07] = (byte)(cfg_iplsize & 0xFF);
-
-            d64_sys_data[0x1C] = (byte)(cfg_ipladdr >> 24);
-            d64_sys_data[0x1D] = (byte)(cfg_ipladdr >> 16);
-            d64_sys_data[0x1E] = (byte)(cfg_ipladdr >> 8);
-            d64_sys_data[0x1F] = (byte)(cfg_ipladdr & 0xFF);
-
-            d64_sys_data[0xE0] = (byte)((block_rom_size - 1) >> 8);
-            d64_sys_data[0xE1] = (byte)((block_rom_size - 1) & 0xFF);
-            d64_sys_data[0xE2] = 0xFF;
-            d64_sys_data[0xE3] = 0xFF;
-            d64_sys_data[0xE4] = 0xFF;
-            d64_sys_data[0xE5] = 0xFF;
-
-            Array.Copy(cfg_diskid, d64_sys_id, cfg_diskid.Length);
-            d64_sys_id[0xE8] = (byte)cfg_destcode;
-
-            file_out.Write(d64_sys_data, 0, d64_sys_data.Length);
-            file_out.Write(d64_sys_id, 0, d64_sys_id.Length);
-            file_out.Write(rom_data, 0, rom_data.Length);
-
-            file_out.Close();
         }
 
         static void ProcessCfg(string filepath, out int disktype, out int destcode, out byte[] diskid)
@@ -314,13 +400,17 @@ namespace ddmaster
             Console.WriteLine("Usage:");
             Console.WriteLine(" ddmaster <arguments>");
             Console.WriteLine("Arguments:");
+            Console.WriteLine("Make Disk with Config:");
             Console.WriteLine(" -cfg <filepath> = Use file as Disk Info configuration (required)");
             Console.WriteLine(" -rom <filepath> = Use file as the Disk ROM content (required)");
             //Console.WriteLine(" -ram <filepath> = Use file as the default Disk RAM content (optional)");
             Console.WriteLine(" -ipladdr <RAM address> = Start RAM Address of the main boot code in hex");
             Console.WriteLine(" -iplsize <Size> = Size in bytes (decimal) of the main boot code");
+            Console.WriteLine("");
+            Console.WriteLine("Make Disk with NDD Image:");
+            Console.WriteLine(" -ndd <filepath> = Use file as retail disk file to convert to d64 (required)");
+            Console.WriteLine("");
             Console.WriteLine(" -o <filepath> = Use filepath as the output disk file (optional, will make master.d64 by default)");
-            //Console.WriteLine(" -ndd <filepath> = Use file as retail disk file to convert to d64");
         }
 
         static byte[] SECTOR_SIZES = { 0xE8, 0xD8, 0xD0, 0xC0, 0xB0, 0xA0, 0x90, 0x80, 0x70 };
